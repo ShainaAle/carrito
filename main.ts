013d722a82fd498354bb81e9b9bb37a5342b0195
ShainaAle <124312507+ShainaAle@users.noreply.github.com>
2023-11-06 23:01:35 -0600

    En teroria queda la semana 2

--- main.ts
+++ main.ts
@@ -1,5 +1,6 @@
 let estado = ""
 let prendido = 0
+let numSer = 0
 basic.forever(function () {
     estado = serial.readUntil(serial.delimiters(Delimiters.NewLine))
     if (estado == "encender") {
@@ -7,13 +8,14 @@ basic.forever(function () {
         prendido = 1
     }
     if (estado == "apagar") {
-        images.createImage(`
-            . . . . .
-            . . . . .
-            . . . . .
-            . . . . .
-            . . . . .
-            `).showImage(0)
+        basic.clearScreen()
         prendido = 0
+    }
+    numSer = parseFloat(estado.substr(0, 3))
+    if (prendido == 1) {
+        cuteBot.moveTime(cuteBot.Direction.forward, numSer, 10)
+        cuteBot.colorLight(cuteBot.RGBLights.ALL, 0x00ffff)
+    } else {
+        cuteBot.colorLight(cuteBot.RGBLights.ALL, 0xff00ff)
     }
 })
