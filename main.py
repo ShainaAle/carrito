2a2198770c9fd5559d75aecfc24c188f0385d8a5
ShainaAle <124312507+ShainaAle@users.noreply.github.com>
2023-11-27 21:18:08 -0600

    se elimino el frenado automatico

--- main.py
+++ main.py
@@ -1,21 +1,76 @@
-estado = ""
 prendido = 0
 numSer = 0
+estado = ""
+objeto = 0
+
+def on_bluetooth_connected():
+    images.icon_image(IconNames.YES).show_image(0)
+    bluetooth.start_uart_service()
+bluetooth.on_bluetooth_connected(on_bluetooth_connected)
+
+def on_bluetooth_disconnected():
+    images.icon_image(IconNames.NO).show_image(0)
+    cuteBot.stopcar()
+bluetooth.on_bluetooth_disconnected(on_bluetooth_disconnected)
+
+def Apagar():
+    global prendido
+    basic.clear_screen()
+    prendido = 0
+    cuteBot.closeheadlights()
+    return prendido
+def Encender():
+    global prendido
+    images.icon_image(IconNames.GIRAFFE).show_image(0)
+    prendido = 1
+    cuteBot.color_light(cuteBot.RGBLights.ALL, 0x00ffff)
+    return prendido
+def Velocidad():
+    global numSer
+    numSer = parse_float(estado)
+    if prendido == 0:
+        cuteBot.stopcar()
+    return numSer
 
 def on_forever():
-    global estado, prendido, numSer
-    estado = serial.read_until(serial.delimiters(Delimiters.NEW_LINE))
+    global estado, objeto
+    estado = bluetooth.uart_read_until(serial.delimiters(Delimiters.NEW_LINE))
+    objeto = cuteBot.ultrasonic(cuteBot.SonarUnit.CENTIMETERS)
     if estado == "encender":
-        images.icon_image(IconNames.GIRAFFE).show_image(0)
-        prendido = 1
+        Encender()
     if estado == "apagar":
-        basic.clear_screen()
-        prendido = 0
-    numSer = parse_float(estado)
-    if prendido == 1:
-        cuteBot.color_light(cuteBot.RGBLights.ALL, 0x00ffff)
-        cuteBot.motors(numSer, numSer)
-    else:
-        cuteBot.color_light(cuteBot.RGBLights.ALL, 0xff00ff)
+        Apagar()
+    Velocidad()
+    if estado == "arriba":
+        cuteBot.move_time(cuteBot.Direction.FORWARD, numSer, 1)
+    if estado == "abajo":
+        cuteBot.move_time(cuteBot.Direction.BACKWARD, numSer, 1)
+        music.play(music.create_sound_expression(WaveShape.SQUARE,
+                2672,
+                1542,
+                255,
+                245,
+                1000,
+                SoundExpressionEffect.VIBRATO,
+                InterpolationCurve.LOGARITHMIC),
+            music.PlaybackMode.UNTIL_DONE)
+    if estado == "derecha":
+        cuteBot.move_time(cuteBot.Direction.RIGHT, numSer, 1)
+        cuteBot.color_light(cuteBot.RGBLights.RGB_R, 0xffff00)
+    if estado == "izquierda":
+        cuteBot.move_time(cuteBot.Direction.LEFT, numSer, 1)
+        cuteBot.color_light(cuteBot.RGBLights.RGB_L, 0xffff00)
+    if objeto <= 15:
         cuteBot.stopcar()
+        cuteBot.color_light(cuteBot.RGBLights.ALL, 0xff0000)
+        music.play(music.create_sound_expression(WaveShape.SQUARE,
+                4322,
+                4451,
+                255,
+                255,
+                5000,
+                SoundExpressionEffect.VIBRATO,
+                InterpolationCurve.CURVE),
+            music.PlaybackMode.UNTIL_DONE)
+    bluetooth.uart_write_number(objeto)
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,30 +1,16 @@
-let prendido = 0
-let numSer = 0
 let estado = ""
 let objeto = 0
+let numSer = 0
+let prendido = 0
 bluetooth.onBluetoothConnected(function () {
     images.iconImage(IconNames.Yes).showImage(0)
+    bluetooth.startUartService()
 })
-function Apagar () {
-    basic.clearScreen()
-    prendido = 0
-    cuteBot.closeheadlights()
-    return prendido
-}
-function Encender () {
-    images.iconImage(IconNames.Giraffe).showImage(0)
-    prendido = 1
-    cuteBot.colorLight(cuteBot.RGBLights.ALL, 0x00ffff)
-    return prendido
-}
-function Velocidad () {
-    numSer = parseFloat(estado)
-    if (prendido == 0) {
-        cuteBot.stopcar()
-    }
-    return numSer
-}
-basic.forever(function () {
+bluetooth.onBluetoothDisconnected(function () {
+    images.iconImage(IconNames.No).showImage(0)
+    cuteBot.stopcar()
+})
+bluetooth.onUartDataReceived(serial.delimiters(Delimiters.NewLine), function () {
     estado = bluetooth.uartReadUntil(serial.delimiters(Delimiters.NewLine))
     objeto = cuteBot.ultrasonic(cuteBot.SonarUnit.Centimeters)
     if (estado == "encender") {
@@ -50,9 +36,31 @@ basic.forever(function () {
         cuteBot.colorLight(cuteBot.RGBLights.RGB_L, 0xffff00)
     }
     if (objeto <= 15) {
-        cuteBot.stopcar()
         cuteBot.colorLight(cuteBot.RGBLights.ALL, 0xff0000)
         music.play(music.createSoundExpression(WaveShape.Square, 4322, 4451, 255, 255, 5000, SoundExpressionEffect.Vibrato, InterpolationCurve.Curve), music.PlaybackMode.UntilDone)
     }
     bluetooth.uartWriteNumber(objeto)
+})
+function Apagar () {
+    basic.clearScreen()
+    prendido = 0
+    cuteBot.closeheadlights()
+    return prendido
+}
+function Encender () {
+    images.iconImage(IconNames.Giraffe).showImage(0)
+    prendido = 1
+    cuteBot.colorLight(cuteBot.RGBLights.ALL, 0x00ffff)
+    return prendido
+}
+function Velocidad () {
+    numSer = parseFloat(estado)
+    if (prendido == 0) {
+        cuteBot.stopcar()
+    }
+    return numSer
+}
+basic.forever(function () {
+    bluetooth.startUartService()
+    bluetooth.setTransmitPower(1)
 })
